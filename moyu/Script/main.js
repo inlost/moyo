398d0a6b2c1c4491152487c82bec99941bd9bbcc
laobai <boodleu@qq.com>
2012-04-15 11:33:36 +0800

    Signed-off-by: laobai <boodleu@qq.com>

--- moyu/Script/main.js
+++ moyu/Script/main.js
@@ -30,9 +30,16 @@ Moyo.prototype.home={
     channelHover:function(){
         var channels=$(".channelItem");
         channels.hover(function(){
+            var channelName=$(this).children(".channelName");
+            channelName.css({"display":"block",
+                                "background-color":$(this).css("background-color")});
             $(this).animate({"opacity":"1"},500);
         },function(){
-            $(this).animate({"opacity":"0.6"},100);
+            var channelName=$(this).children(".channelName");
+            channelName.css("display","none");
+            if(!$(this).hasClass("activeChannel")){
+                $(this).animate({"opacity":"0.6"},100);
+            }
         });
     },
     focusOnWhat:function(){
@@ -40,11 +47,68 @@ Moyo.prototype.home={
         $("#content").on("mousemove",function(e){
             pointer.css("left",e.pageX-28);
         });
+    },
+    channelClick:function(){
+        var channels=$(".channelItem");
+        var header=$("#header");
+        var splitBar=$("#splitBar");
+        var market=$("#market");
+        var channelContent=$("#marketContent");
+        channels.on("click",function(){
+            var nowChannel=$(this);
+            splitBar.css("display","none");
+            header.slideUp("normal");
+            channels.each(function(){
+               nowChannel.removeClass("activeChannel");
+            });
+            nowChannel.addClass("activeChannel");
+            market.removeClass("hide");
+            $.ajax({
+                url:getUrl(nowChannel)+"index.html",
+                type:"GET",
+                success:function(msg){
+                    $("#market-status-bar").hide();
+                    channelContent.html(msg);
+                    channelContent.parent().removeClass("hide").show();
+                },error:function(e){
+                    console.log(e);
+                }
+            });
+            return false;
+        });
+        function getUrl(channelElm)
+        {
+            switch(channelElm.attr("id"))
+            {
+                case "cpChannel":
+                    return "../Markets/Computer/";
+            }
+        }
+    },
+    switchToSubForm:function(formFor,data){
+        var content=$("#marketContent");
+        var subForm=$("#subForm");
+        var formType=$("#formType");
+        content.addClass("marketContentOnSubmit").animate({left:"-3000"},"normal",function(){
+            subForm.fadeIn("slow");
+            formType.val(formFor);
+        });
+    }
+};
+Moyo.prototype.computer={
+    bindSubmit:function(){
+        var submitButton=$(".submitForm");
+        submitButton.on("click",function(){
+            var nowClick=$(this);
+            moyo.home.switchToSubForm(nowClick.attr("data-for"));
+            return false;
+        });
     }
 };
 $(document).ready(function(){
-    var moyo=new Moyo();
+    moyo=new Moyo();
     moyo.home.headImgFloat();
     moyo.home.channelHover();
     moyo.home.focusOnWhat();
+    moyo.home.channelClick();
 });
